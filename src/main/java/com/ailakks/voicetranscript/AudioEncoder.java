e94f970c20fc9b0a9e47a95ff043ff91590364e7
Ailakks <hola@cortmnzz.com>
2023-04-04 15:58:58 +0200

    added audio encoder

---
+++ src/main/java/com/ailakks/voicetranscript/AudioEncoder.java
@@ -0,0 +1,39 @@
+/**
+ * This code has been programmed by Ailakks.
+ * Please, leave this note and give me credits
+ * in any project in which it is used. Have a nice day!
+ *
+ * @author : Ailakks
+ * @mailto : hola@ailakks.com
+ * @created : 04/04/2023
+ */
+
+package com.ailakks.voicetranscript;
+
+import net.sourceforge.lame.lowlevel.LameEncoder;
+import net.sourceforge.lame.mp3.Lame;
+import net.sourceforge.lame.mp3.MPEGMode;
+
+import java.io.ByteArrayOutputStream;
+
+public class AudioEncoder {
+    public static byte[] encode(byte[] pcm) {
+        LameEncoder encoder = new LameEncoder(new javax.sound.sampled.AudioFormat(44100.0f, 16, 2, true, false), 256, MPEGMode.STEREO, Lame.QUALITY_HIGHEST, false);
+
+
+        ByteArrayOutputStream mp3 = new ByteArrayOutputStream();
+        byte[] buffer = new byte[encoder.getPCMBufferSize()];
+
+        int bytesToTransfer = Math.min(buffer.length, pcm.length);
+        int bytesWritten;
+        int currentPcmPosition = 0;
+
+        while (0 < (bytesWritten = encoder.encodeBuffer(pcm, currentPcmPosition, bytesToTransfer, buffer))) {
+            currentPcmPosition += bytesToTransfer;
+            bytesToTransfer = Math.min(buffer.length, pcm.length - currentPcmPosition);
+            mp3.write(buffer, 0, bytesWritten);
+        }
+
+        return mp3.toByteArray();
+    }
+}
